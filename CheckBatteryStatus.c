a24193e6a2cafc716676949329ffab817353111e
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-24 19:42:13 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "MonitorBattery.h"
 
+int main() {
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+}
