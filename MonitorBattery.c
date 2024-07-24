6f22b98df623d2efe34fec916c59da04148b4f63
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-24 19:31:35 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 
+bool tempIsOk(float temperature){ 
+  if(temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return false;
+	}
+	return true;
+}
+
+bool socIsOk(float soc){ 
+  if(soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return false;
+  }
+  return true;
+}
+
+bool chargeRateIsOk(float chargeRate){ 
+  if(chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
+    return false;
+  }
+  return true;
+ 
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+return (tempIsOk(temperature) && (socIsOk(soc) && (chargeRateIsOk(chargeRate))));
+}
