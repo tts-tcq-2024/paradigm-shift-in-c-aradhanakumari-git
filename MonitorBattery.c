0a0f0ad2dc65c5a1a50316ca784d47959c6b952b
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 23:27:20 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -9,37 +9,29 @@
 bool tempIsOk(float temperature){ 
   if(temperature < 0 || temperature > 45) {
     printMsg("Temperature out of range!\n");
-    //printf("Temperature out of range!\n");
     return false;}
   else if (temperature <= TEMP_LOWER_LIMIT + TEMP_WARNING_THRESHOLD) {
 	  printMsg("warning: Temperature lower limit warning!\n");
-	  //printf("warning: Temperature lower limit warning!\n");
   } else if (temperature >= TEMP_UPPER_LIMIT - TEMP_WARNING_THRESHOLD) {
-	  printMsg("warning: Temperature upper limit warning!\n");
-	  //printf("warning: Temperature upper limit warning!\n");}
+	  printMsg("warning: Temperature upper limit warning!\n");}
 return true;}
 
 bool socIsOk(float soc){
     if (soc < SOC_LOWER_LIMIT || soc > 	SOC_UPPER_LIMIT) {
 	printMsg("State of Charge out of range!\n");
-         //printf("State of Charge out of range!\n");
         return false;}
     else if (soc <= SOC_LOWER_LIMIT + SOC_WARNING_THRESHOLD) {
-	    printMsg("Warning : SOC Lower limit warning!\n");
-	    //printf("Warning : SOC Lower limit warning!\n");}
+	    printMsg("Warning : SOC Lower limit warning!\n");}
     else if (soc >= SOC_UPPER_LIMIT - SOC_WARNING_THRESHOLD) {
-	    printMsg("Warning : SOC Upper limit warning!\n");
-	    //printf("Warning : SOC Upper limit warning!\n");}
+	    printMsg("Warning : SOC Upper limit warning!\n");}
     return true;}
 
 bool chargeRateIsOk(float chargeRate){ 
   if(chargeRate > 0.8) {
     printMsg("Charge Rate out of range!\n");
-    //printf("Charge Rate out of range!\n");
     return false;}
   else if(chargeRate >= 0.8 - CHARGE_RATE_WARNING_THRESHOLD){
-	  printMsg("Warning : Charge Rate upper limit warning!\n");
-	  //printf("Warning : Charge Rate upper limit warning!\n");}
+	  printMsg("Warning : Charge Rate upper limit warning!\n");}
   return true;}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
