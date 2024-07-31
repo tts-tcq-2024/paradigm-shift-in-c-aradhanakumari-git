fb95f2ce22bfaf142b7d6688b271c154773ba09a
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 23:48:31 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -8,7 +8,7 @@ int main() {
   assert(!batteryIsOk(50, 85, 0));
   assert(!batteryIsOk(1, 85, 0.7)); // warning for temp low
   assert(!batteryIsOk(44, 85, 0.7)); // warning for temp high
-  //assert(!batteryIsOk(30, 23, 0.7)); // warning for low soc
-  assert(!batteryIsOk(30, 79, 0.7)); // warning for high soc
-  assert(!batteryIsOk(30, 70, 0.7)); // warning for high charge rate
+  assert(batteryIsOk(30, 23, 0.7)); // warning for low soc
+  assert(batteryIsOk(30, 79, 0.7)); // warning for high soc
+  assert(batteryIsOk(30, 70, 0.7)); // warning for high charge rate
 }
