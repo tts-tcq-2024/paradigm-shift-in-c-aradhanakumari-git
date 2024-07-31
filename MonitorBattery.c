ca52c92793eceee20874c1e5133d6624ef9b2203
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 23:07:37 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -2,15 +2,6 @@
 #include <assert.h>
 #include <stdbool.h>
 
-#define TEMP_UPPER_LIMIT 		45
-#define TEMP_LOWER_LIMIT 		0
-#define SOC_UPPER_LIMIT			80
-#define SOC_LOWER_LIMIT			20
-#define CHARGE_RATE_UPPER_LIMIT         0.8
-
-#define TEMP_WARNING_THRESHOLD          0.05 * TEMP_UPPER_LIMIT
-#define SOC_WARNING_THRESHOLD           0.05 * SOC_UPPER_LIMIT
-#define CHARGE_RATE_WARNING_THRESHOLD   0.05 * CHARGE_RATE_UPPER_LIMIT
 
 
 bool tempIsOk(float temperature){ 
