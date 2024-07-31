12d2642297387ce7859859784b6919d8d8b406ac
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 23:03:16 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -4,17 +4,13 @@
 
 #define TEMP_UPPER_LIMIT 		45
 #define TEMP_LOWER_LIMIT 		0
-
 #define SOC_UPPER_LIMIT			80
 #define SOC_LOWER_LIMIT			20
+#define CHARGE_RATE_UPPER_LIMIT         0.8
 
-#define CHARGE_RATE_UPPER_LIMIT         1
-#define CHARGE_RATE_LOWER_LIMIT         0
-
-
-#define TEMP_WARNING_THRESHOLD          0.05 * 45
-#define SOC_WARNING_THRESHOLD           0.05 * 80
-#define CHARGE_RATE_WARNING_THRESHOLD   0.05 * 0.8
+#define TEMP_WARNING_THRESHOLD          0.05 * TEMP_UPPER_LIMIT
+#define SOC_WARNING_THRESHOLD           0.05 * SOC_UPPER_LIMIT
+#define CHARGE_RATE_WARNING_THRESHOLD   0.05 * CHARGE_RATE_UPPER_LIMIT
 
 
 bool tempIsOk(float temperature){ 
@@ -27,16 +23,22 @@ bool tempIsOk(float temperature){
 	  printf("warning: Temperature upper limit warning!\n");}
 return true;}
 
-bool socIsOk(float soc){ 
-  if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return false;}
-  return true;}
+bool socIsOk(float soc){
+    if (soc < SOC_LOWER_LIMIT || soc > 	SOC_UPPER_LIMIT) {
+         printf("State of Charge out of range!\n");
+        return false;}
+    else if (soc <= SOC_LOWER_LIMIT + SOC_WARNING_THRESHOLD) {
+	    printf("Warning : SOC Lower limit warning!\n");}
+    else if (soc >= SOC_UPPER_LIMIT - SOC_WARNING_THRESHOLD) {
+	    printf("Warning : SOC Upper limit warning!\n");}
+    return true;}
 
 bool chargeRateIsOk(float chargeRate){ 
   if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return false;}
+  else if(chargeRate >= 0.8 - CHARGE_RATE_WARNING_THRESHOLD){
+	  printf("Warning : Charge Rate upper limit warning!\n");}
   return true;}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
