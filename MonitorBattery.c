358c71ff37246ce29c4e471a4a5a5e8cd52e7e8e
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 19:38:55 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -2,12 +2,32 @@
 #include <assert.h>
 #include <stdbool.h>
 
+#define TEMP_UPPER_LIMIT 		45
+#define TEMP_LOWER_LIMIT 		0
+
+#define SOC_UPPER_LIMIT			80
+#define SOC_LOWER_LIMIT			20
+
+#define CHARGE_RATE_UPPER_LIMIT         1
+#define CHARGE_RATE_LOWER_LIMIT         0
+
+
+#define TEMP_WARNING_THRESHOLD          0.05 * 45
+#define SOC_WARNING_THRESHOLD           0.05 * 80
+#define CHARGE_RATE_WARNING_THRESHOLD   0.05 * 0.8
+
+
 bool tempIsOk(float temperature){ 
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return false;
 	}
-	return true;
+  else if (temperature <= TEMP_LOWER_LIMIT + TEMPERATURE_WARNING_THRESHOLD) {
+	  printf("warning: Temperature lower limit warning!\n");
+  } else if (temperature >= TEMP_UPPER_LIMIT - TEMPERATURE_WARNING_THRESHOLD) {
+	  printf("warning: Temperature upper limit warning!\n");
+	  return true;
+  }
 }
 
 bool socIsOk(float soc){ 
