fdcb107fee91bf240d2f8e9d2074f930130442fe
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-07-31 20:23:32 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -20,34 +20,24 @@
 bool tempIsOk(float temperature){ 
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
-    return false;
-	}
+    return false;}
   else if (temperature <= TEMP_LOWER_LIMIT + TEMP_WARNING_THRESHOLD) {
 	  printf("warning: Temperature lower limit warning!\n");
   } else if (temperature >= TEMP_UPPER_LIMIT - TEMP_WARNING_THRESHOLD) {
-	  printf("warning: Temperature upper limit warning!\n");
-	 }
-return true;
-}
+	  printf("warning: Temperature upper limit warning!\n");}
+return true;}
 
 bool socIsOk(float soc){ 
   if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
-    return false;
-  }
-  return true;
-}
+    return false;}
+  return true;}
 
 bool chargeRateIsOk(float chargeRate){ 
   if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
-    return false;
-  }
-  return true;
- 
-}
+    return false;}
+  return true;}
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-return (tempIsOk(temperature) && (socIsOk(soc) && (chargeRateIsOk(chargeRate))));
-}
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+return (tempIsOk(temperature) && (socIsOk(soc) && (chargeRateIsOk(chargeRate))));}
