64a3a9f5d852bf9062d4c82ce34da00ef2953d8e
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-07 23:58:47 +0530

    Update MonitorBattery.h

--- MonitorBattery.h
+++ MonitorBattery.h
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#define TEMP_UPPER_LIMIT 		45
+#define TEMP_LOWER_LIMIT 		0
+#define SOC_UPPER_LIMIT			80
+#define SOC_LOWER_LIMIT			20
+#define CHARGE_RATE_UPPER_LIMIT         0.8
 
+typedef enum 
+{
+ENGLISH,
+HINDI,
+GERMAN
+} Language;
+
+Language language_e;
 
 bool batteryIsOk(float temperature, float soc, float chargeRate);
 
