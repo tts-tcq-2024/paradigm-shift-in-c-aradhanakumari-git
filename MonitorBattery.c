92dd75d445b8b4bda3c3743c7478ac30c94463fc
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-07 21:46:30 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -15,35 +15,6 @@
 #define SOC_WARNING_THRESHOLD           0.05 * SOC_UPPER_LIMIT
 #define CHARGE_RATE_WARNING_THRESHOLD   0.05 * CHARGE_RATE_UPPER_LIMIT
 
-
-// bool tempIsOk(float temperature){ 
-//   if(temperature < 0 || temperature > 45) {
-//     printMsg("Temperature out of range!\n");
-//     return false;}
-//   else if (temperature <= TEMP_LOWER_LIMIT + TEMP_WARNING_THRESHOLD) {
-// 	  printMsg("warning: Temperature lower limit warning!\n");
-//   } else if (temperature >= TEMP_UPPER_LIMIT - TEMP_WARNING_THRESHOLD) {
-// 	  printMsg("warning: Temperature upper limit warning!\n");}
-// return true;}
-
-// bool socIsOk(float soc){
-//     if (soc < SOC_LOWER_LIMIT || soc > 	SOC_UPPER_LIMIT) {
-// 	printMsg("State of Charge out of range!\n");
-//         return false;}
-//     else if (soc <= SOC_LOWER_LIMIT + SOC_WARNING_THRESHOLD) {
-// 	    printMsg("Warning : SOC Lower limit warning!\n");}
-//     else if (soc >= SOC_UPPER_LIMIT - SOC_WARNING_THRESHOLD) {
-// 	    printMsg("Warning : SOC Upper limit warning!\n");}
-//     return true;}
-
-// bool chargeRateIsOk(float chargeRate){ 
-//   if(chargeRate > 0.8) {
-//     printMsg("Charge Rate out of range!\n");
-//     return false;}
-//   else if(chargeRate >= 0.8 - CHARGE_RATE_WARNING_THRESHOLD){
-// 	  printMsg("Warning : Charge Rate upper limit warning!\n");}
-//   return true;}
-
 int evaluateBatteryParam(const char* param, int value, int upper_limit, int lower_limit)
 {
 	int ret = 1;
@@ -61,7 +32,6 @@ int evaluateBatteryParam(const char* param, int value, int upper_limit, int lowe
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
 return (evaluateBatteryParam("temperature",temperature,45,0) && (evaluateBatteryParam("soc",soc,80,20) && (evaluateBatteryParam("chargeRate",chargeRate,0.8,0))));
-
 //return (tempIsOk(temperature) && (socIsOk(soc) && (chargeRateIsOk(chargeRate))));
 }
 
