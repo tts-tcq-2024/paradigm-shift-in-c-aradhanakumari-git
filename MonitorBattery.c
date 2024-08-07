72cd40d3ac1f27668e77778a89aa1d98079068c3
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-07 23:58:35 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -5,20 +5,7 @@
 
 #include "MonitorBattery.h"
 
-#define TEMP_UPPER_LIMIT 		45
-#define TEMP_LOWER_LIMIT 		0
-#define SOC_UPPER_LIMIT			80
-#define SOC_LOWER_LIMIT			20
-#define CHARGE_RATE_UPPER_LIMIT         0.8
 
-typedef enum 
-{
-ENGLISH,
-HINDI,
-GERMAN
-} Language;
-
-Language language_e;
 int evaluateBatteryParam(const char* param, int value, int upper_limit, int lower_limit)
 {
 	int ret = 1;
