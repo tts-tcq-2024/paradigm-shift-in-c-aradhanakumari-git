a8e1f412d0e6ce9fae41b14ce52fa152a5110c26
aradhanakumari-git <aradhana.kumari2710@gmail.com>
2024-08-08 00:06:21 +0530

    Update MonitorBattery.c

--- MonitorBattery.c
+++ MonitorBattery.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <string.h>
 
-//#include "MonitorBattery.h"
 #include "printMessage.h"
 
 
@@ -22,7 +21,6 @@ int evaluateBatteryParam(const char* param, int value, int upper_limit, int lowe
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
 return (evaluateBatteryParam("temperature",temperature,TEMP_UPPER_LIMIT,TEMP_LOWER_LIMIT) && (evaluateBatteryParam("soc",soc,SOC_UPPER_LIMIT,SOC_LOWER_LIMIT) && (evaluateBatteryParam("chargeRate",chargeRate,CHARGE_RATE_UPPER_LIMIT,0))));
-//return (tempIsOk(temperature) && (socIsOk(soc) && (chargeRateIsOk(chargeRate))));
 }
 
 
